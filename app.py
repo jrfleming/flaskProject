0c9c8a9c154b21d9f5414901bc0233af3addef84
jrfleming <91241750+jrfleming@users.noreply.github.com>
2022-04-25 20:13:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,13 @@
 from flask import Flask, render_template
 from typing import DefaultDict
 from datetime import datetime
-import sqlite3
-from sqlite3 import Error
+##import sqlite3
+##from sqlite3 import Error
+import mysql.connector
+from mysql.connector import Error
 import os
 
+DATABASENAME = "inboxProject6"  # PUT THE DATABASE NAME HERE,  when you know it
 
 project_root = os.path.dirname(__file__)
 # define the templates directory where you keep your html files that get rendered
@@ -34,6 +37,8 @@ def command(cmd=None):
     # main.html  has user select a command, to read email, write email, etc.  Process the command.
     username = cmd.split(")(")[0]  # Use the parens as a separator (cos users unlikely to have )( in their user name
     cmd = cmd.split(")(")[1]
+    username = username.upper()
+
     if cmd == READEMAIL:
         response = "Reading email for " + username
 
@@ -46,6 +51,8 @@ def command(cmd=None):
         return response, 200, {'Content-Type': 'text/plain'}
     elif cmd == SENDEMAIL:
         response = "Compose a new email"
+
+        render_template('displayemail.html')
         composeemail(username)
         return response, 200, {'Content-Type': 'text/plain'}
     elif cmd == CHECKEMAIL:
@@ -58,46 +65,46 @@ def command(cmd=None):
 
 @app.route('/composeemail', methods=['POST'])
 def composeemail(username):
-    #TODO - write this function
+    print("Entering composeemail python function")
+
+    # Prompt user for inputs
+
+    #  connect to the SQL db
+    connection = create_connection();
+    crsr = connection.cursor();
+
+
+    counter = 0
+
+
+    # close the connection
+    connection.close()
+    return "SHOW THIS INFO"
     return
 
 @app.route('/reademail', methods=['POST'])
 def reademail(username):
     print("Entering reademail python function")
-    username = username.upper()
+
+    email_list = []
     #  connect to the SQL db
-    try:
-        connection = sqlite3.connect("inboxProject3.db")  # TODO FIND db name and location?
-        # set up our cursor to that db
-        crsr = connection.cursor()
-    except Error as e:
-        # If we could not connect to the sql, give a nice error and stop
-        print("Could not attach to the db", e)
-        return "Could not connect to the email database, please try again later"
-    try:
-        crsr.execute("SELECT * FROM messages")
-        connection.commit()
-        # print(crsr.fetchall())  # This is a debugging statement to make sure tables exist in the sql database/file
-    except Error as e:
-        print("no tables in the db to list", e)
+    connection = create_connection();
+    crsr = connection.cursor();
+
     try:
         crsr.execute("SELECT from_user, to_user, date, message, time  from messages")
-    except Error as e:
-        print("Debugging - to_user  not found in messages table for", username, e)
-        return "Could not locate to_user name in the database"
-    response = "No emails found for " + username
-    email_list = []
+    except:
+        print("Debugging - to_user  not found in messages table for", username)
+        email_data = "No emails found for " + username
+        email_list.append(email_data)
+        return email_list
+
+
     myresult = crsr.fetchall()
     counter = 0
     for myrow in myresult:
         if username == myrow[1]:
-            '''email_dict[counter] = []
-            email_dict[counter].append(("TO:" + myrow[1]))
-            email_dict[counter].append(("FROM:" + myrow[0]))
-            email_dict[counter].append(("Date:" + myrow[2]))
-            email_dict[counter].append(myrow[3])
-            print("Debugging - hit line 97", email_dict[counter])
-            counter = counter + 1'''
+
             email_data = "To: " + myrow[1] + " From: " + myrow[0] + " Date: " + myrow[2] + " " + myrow[4] + "\n" + "MSG: " + myrow[3]
             email_list.append(email_data)
 
@@ -105,34 +112,58 @@ def reademail(username):
     connection.close()
     return email_list
 
+def create_connection():
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            passwd="",
+            database=DATABASENAME
+        )
+        print("Connection to MySQL DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred trying to connect to sql db {DATABASENAME}")
+
+    return connection
+
+
+def create_database():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password=""
+    )
+
+    cursor = mydb.cursor()
+    stringtoexecute = "CREATE DATABASE " + DATABASENAME
+    cursor.execute(stringtoexecute)
+
+    return mydb
+
 
 def populatesql():
-    """ This is for testing purposes to populate an SQL db"""
-    conn = None
+     #This is for testing purposes to populate an SQL db
+    cur = None
     try:
-        conn = sqlite3.connect("inboxProject3.db")
-    except Error as e:
-        print(e)
-    cur = conn.cursor()
+        connection = create_database();
+    except:
+        try:
+            connection = create_connection();
+        except Error as e:
+            print("Could not connect or create database {DATABASENAME}  in populate sql, error is {e}")
+
+
+
+    cur = connection.cursor()
+
     try:
-        table = """CREATE TABLE MESSAGES(to_user VARCHAR(32), from_user VARCHAR(32), message VARCHAR(512), 
-        date VARCHAR(30),  time VARCHAR(30));"""
+        table = "CREATE TABLE MESSAGES(to_user VARCHAR(32), from_user VARCHAR(32), message VARCHAR(512), date VARCHAR(30),  time VARCHAR(30))"
         cur.execute(table)
-        conn.commit()
-    except Error as e:
-        print(e)
-        print("messages table exists")
+        connection.commit()
+    except:
+        print("messages table already exists in the sql db")
 
-    cur.execute("SELECT name FROM sqlite_master WHERE type='table'")
-    print("Did messages get created?")
-    print(cur.fetchall())  # This is a debuggin statement to make sure tables exist in the sql database/file
-
-    try:
-        cur.execute("SELECT * FROM messages")
-        conn.commit()
-        print(cur.fetchall())  # This is a debugging statement to make sure tables exist in the sql database/file
-    except Error as e:
-        print("no tables in the db to list", e)
 
     # Put some messages into the messages table (for testing.
     # This code is just to populate SQL, is not going to be in final project
@@ -143,22 +174,22 @@ def populatesql():
     print("date and time", date, time)
 
     try:
-        cur.executemany("INSERT INTO messages (date, time, to_user, from_user, message) VALUES (?, ?, ?, ?, ?)",
+
+        cur.executemany("""INSERT INTO messages(date, time, to_user, from_user, message)
+        VALUES(%s,%s,%s,%s,%s)""",
             [
-                (date, time, "LUKE_S",  "DARTH_V", "Hello Luke I am your Father"),
-                (date, time,  "JRF", "LUKE_S", "Hey Joe, want to go flying to Gondor tomorrow?"),
-                (date, time, "DARTH_V", "JRF", "Dear Mr. Vader,   You suck.   Love, Joe F"),
-                (date, time,  "LUKE_S", "JRF", "Hello Luke, Yes it would be grand to visit.  Joe")
+                (date, time, "LUKE_S",  "DARTH_V", "Hello Luke I am your Father,  Love from Dad"),
+                (date, time,  "LUKE_S", "HAN_S", "Hey Luke, let us grab a beer at The Dickens tomorrow?"),
+                (date, time, "DARTH_V", "JRF", "Dear Mr. V,  You suck.   From, Joe F"),
+                (date, time, "LUKE_S", "JRF", "Hello Luke, Yes it would be grand to visit.  Joe"),
+                (date, time,  "JRF", "DARTH_V", "No, you! -- Darth ")
             ]
         )
     except Error as e:
-        print("already created the sql db, error is ", e)
+        print("already created the sql db, - OR - error is ", e)
 
-    conn.commit()
-    cur.execute("SELECT name FROM sqlite_master WHERE type='table'")
-
-    print(cur.fetchall())  # This is a debuggin statement to make sure tables exist in the sql database/file
-    conn.close()
+    connection.commit()
+    connection.close()
 
 
 if __name__ == "__main__":
